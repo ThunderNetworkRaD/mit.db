67730845a5003ed90e75239556e4f0dcca8281be
Killer Boss Original <73131550+killerbossoriginal@users.noreply.github.com>
2022-07-05 16:09:45 +0200

    .

--- index.js
+++ index.js
@@ -4,6 +4,30 @@ client.config = require('./config.js').settings;
 const MapDB = require('quickmap.db');
 const db = new MapDB('database.json');
 
+const chalk = require('chalk')
+
+function catchError(message, err, origin, reason) { 
+  console.log(chalk.gray('—————————————————————————————————'));
+	console.log(
+		chalk.white('['),
+		chalk.red.bold('AntiCrash'),
+		chalk.white(']'),
+		chalk.gray(' : '),
+		chalk.white.bold(message),
+	);
+	console.log(chalk.gray('—————————————————————————————————'));
+	console.log(err, origin, reason);
+}
+
+process.on('unhandledRejection', (err, origin) => {
+    catchError('Unhandled Rejection/Catch', err, origin);
+});
+process.on('uncaughtException', (err, origin) => {
+    catchError('Uncaught Exception/Catch', err, origin);
+});
+process.on('multipleResolves', (type, promise, reason) => {
+    catchError('Multiple Resolves', type, promise, reason);
+});
 
 async function setup() {
     var isset = await db.get('client.issetupped')
